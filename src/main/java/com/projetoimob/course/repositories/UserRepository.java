1ff88630154f3a8554eb88dba1743fb360c8b7b1
Higor Rangel <higorrg2000@hotmail.com>
2022-12-31 17:31:47 -0300

    Service layer, component registration

--- src/main/java/com/projetoimob/course/repositories/UserRepository.java
+++ src/main/java/com/projetoimob/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetoimob.course.entities.User;
 
+//o @Repository não é obrigatório porque já está herdando do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
--- src/main/java/com/projetoimob/course/resources/UserResource.java
+++ src/main/java/com/projetoimob/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.projetoimob.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoimob.course.entities.User;
+import com.projetoimob.course.services.UserService;
 
 //Recurso web  que é implementado por um controlador REST 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Teste", "teste@email.com", "094235892", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findUserById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
